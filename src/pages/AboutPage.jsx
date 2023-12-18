ed57ecaa7a68fd6418407a6377087b430cbb0139
mac <mac@macs-MacBook-Pro-2.local>
2023-12-18 15:26:18 +0100

    first commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h2 className='text-success fw-light '>About Us</h2>
+        <p className='my-3 lh-lg '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sed ipsam voluptates commodi repudiandae dignissimos ullam aperiam cum at itaque rerum deleniti corrupti necessitatibus temporibus, rem eius. Eius corporis nam neque similique possimus! Sequi delectus illo esse quibusdam quae eius.</p>
+
+        <h2 className='text-success fw-light '>What We Do</h2>
+        <p className='my-3 lh-lg '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sed ipsam voluptates commodi repudiandae dignissimos ullam aperiam cum at itaque rerum deleniti corrupti necessitatibus temporibus, rem eius. Eius corporis nam neque similique possimus! Sequi delectus illo esse quibusdam quae eius.</p>
+
+        <h2 className='text-success fw-light '>Our Mission</h2>
+        <p className='my-3 lh-lg '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sed ipsam voluptates commodi repudiandae dignissimos ullam aperiam cum at itaque rerum deleniti corrupti necessitatibus temporibus, rem eius. Eius corporis nam neque similique possimus! Sequi delectus illo esse quibusdam quae eius.</p>
+
+        <h2 className='text-success fw-light '>Our Vision</h2>
+        <p className='my-3 lh-lg '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sed ipsam voluptates commodi repudiandae dignissimos ullam aperiam cum at itaque rerum deleniti corrupti necessitatibus temporibus, rem eius. Eius corporis nam neque similique possimus! Sequi delectus illo esse quibusdam quae eius.</p>
+        
+    </div>
+  )
+}
+
+export default AboutPage
